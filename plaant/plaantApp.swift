8d3c408777fe2420921176d30d463f3f4085dc7b
Raahaf-6 <raahafyy@gmail.com>
2024-10-28 11:11:11 +0300

    Initial Commit

---
+++ plaant/plaantApp.swift
@@ -0,0 +1,17 @@
+//
+//  plaantApp.swift
+//  plaant
+//
+//  Created by Rahaf on 28/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct plaantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ plaantTests/plaantTests.swift
@@ -0,0 +1,17 @@
+//
+//  plaantTests.swift
+//  plaantTests
+//
+//  Created by Rahaf on 28/10/2024.
+//
+
+import Testing
+@testable import plaant
+
+struct plaantTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
